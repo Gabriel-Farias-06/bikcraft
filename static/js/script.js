a3b5cf1b1ceeec64c88b995b627b77fe159d4d42
Gabriel do Rego Farias <gabriel.rfarias@a.ucb.br>
2024-12-04 18:32:30 -0300

    js completo e otimizações para produção feita

--- static/js/script.js
+++ static/js/script.js
@@ -19,3 +19,38 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle("ativa");
+
+  const ativa = resposta.classList.contains("ativa");
+
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventoPergunta(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventoPergunta);
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const width = matchMedia("(min-width: 1000px)").matches;
+  if (width) {
+    galeriaContainer.prepend(img);
+    galeriaContainer.classList.add("cursor-ativo");
+  } else galeriaContainer.classList.remove("cursor-ativo");
+}
+
+galeria.forEach((img) => {
+  img.addEventListener("click", trocarImagem);
+});
